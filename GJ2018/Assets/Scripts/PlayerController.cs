b08e478350b6d1cb2ed17bcf7083ef6b7197f602
Unknown <aws140230@utdallas.edu>
2018-04-07 20:10:28 -0500

    Unity project started, Input for 4 controllers implemented
    See Player Controller & TopDownHandler for info on how that works

---
+++ GJ2018/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public int controller = 1; //Which controller is the player using? 1-4
+	public float speed = 1; //Player speed. Multiplier; 1 = normal, 0.5 = half speed, etc.
+	public float size = 1; //Player size/scale. Multiplier
+	public float density = 1; //Player weight/mass. Multiplier
+	public float cool = 0; //Player coolness. Not a multiplier.
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public Vector2 InputJoyStick(){
+		//Left Joystick
+		//Calculate X axis
+		float xInput = 0.0f;
+		xInput += Input.GetAxis ("Joy" + controller + "X");
+
+		//Calculate Y axis
+		float yInput = 0.0f;
+		yInput += Input.GetAxis ("Joy" + controller + "Y");
+
+		return new Vector2 (xInput, yInput);
+
+	}
+	public bool InputButtonA(){
+		//Button A
+		return Input.GetButtonDown ("Joy" + controller + "A");
+	}
+	public bool InputButtonB(){
+		//Button B
+		return Input.GetButtonDown ("Joy" + controller + "B");
+	}
+}
---
+++ GJ2018/Assets/Scripts/TopDownHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownHandler : MonoBehaviour {
+
+	public PlayerController myPlayer; //The player controlling this game object.
+									  //Currently must be set in inspector, but will fix that.
+	Rigidbody2D myRB;
+
+	// Use this for initialization
+	void Start () {
+		myRB = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		getInput ();
+	}
+
+	void getInput(){
+		//A Button
+		if (myPlayer.InputButtonA()) {
+			Debug.Log ("A Button on controller " + myPlayer.controller + " Pressed.");
+		}
+		if (myPlayer.InputButtonB()) {
+			Debug.Log ("B Button on controller " + myPlayer.controller + " Pressed.");
+		}
+		Debug.Log ("Left Joystick on controller " + myPlayer.controller + " Pressed: " + myPlayer.InputJoyStick());
+		myRB.AddForce (myPlayer.InputJoyStick() * myPlayer.speed);
+	}
+}
